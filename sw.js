128b25a0cd0dcb577255ca787f868c08c29bbcc4
sgk-tools <ss0047740@gmail.com>
2026-06-06 15:29:04 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'btc-edv-minimal-v53';
+const CACHE_NAME = 'btc-edv-minimal-v54';
 const ASSETS = ['./index.html','./manifest.webmanifest'];
 self.addEventListener('install', event => {event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate', event => {event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));self.clients.claim();});
